c83d20c090a6dc337a54ca9284a3821f1a4fb20f
Claudia Diez <claudia_diez@outlook.com>
2022-05-09 13:17:22 +0200

    añadir funcion cuadrado

---
+++ index.js
@@ -0,0 +1,21 @@
+function sum (x,y){
+    console.log(x + y)
+}
+
+function subs (x,y){
+    console.log(x - y)
+}
+
+function multi (x,y){
+    console.log(x * y)
+}
+
+function div (x,y){
+    console.log(x / y)
+}
+
+function cuad (num){
+    console.log(num*num)
+}
+
+module.exports = {sum,subs,multi,div,cuad}
---
+++ run.js
@@ -0,0 +1,6 @@
+let myLib = require("./index");
+
+myLib.sum(6,2);
+myLib.subs(4,1);
+myLib.multi(6,7);
+myLib.div(9,2);
